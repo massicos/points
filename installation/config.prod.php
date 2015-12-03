76bb52bc72fd560c3031acbc7a5b79372210e48f
Steve Massicotte <steve.massicotte@umontreal.ca>
2015-12-02 22:31:27 -0500

    Corrections pour l'installation.

--- installation/config.prod.php
+++ installation/config.prod.php
@@ -1,9 +1,6 @@
 <?php
 
 define('CHEMIN_DESTINATION', '/var/www/points');
-<<<<<<< HEAD
 define('CHEMIN_DESTINATION_PHP', '/app/points-prod');
 define('USAGER_APACHE', 'www-data');
-=======
 define('CHEMIN_DESTINATION_PHP', '/app/points-prod');
->>>>>>> cb066fb76c600b85a47b9a982337c066ef2c7094
